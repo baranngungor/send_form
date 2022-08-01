20aa2845c63ecbb0e0af1c6f4abcbdf56cae23fa
baranngungor <baran.gungor@easycep.com>
2022-08-01 16:21:34 +0300

    export

--- lib/send_form.dart
+++ lib/send_form.dart
@@ -1,5 +1,6 @@
 library send_form;
 
 export 'package:send_form/model/field_model.dart';
+export 'package:send_form/model/dropdown_model.dart';
 export 'package:send_form/controller/send_form_controller.dart';
 export 'package:send_form/view/send_form_view.dart';
